6d61015b6fd144707b03903820345003e543b8f8
gydalo <gylo_l@hotmail.com>
2024-11-14 14:50:26 +0100

    Register and Login now disappear when logged in

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,32 +1,40 @@
+import { isLoggedIn } from "../../api/auth/key.js";
+
+
 // For showing either login or register on homepage:
 document.addEventListener("DOMContentLoaded", function () {
-  let create = document.getElementById("linkRegisterForm");
-  let haveAcc = document.getElementById("linkLogin");
-  let formLogin = document.getElementById("loginForm");
-  let formCreateAcc = document.getElementById("registerForm");
+    const formLogin = document.getElementById("loginForm");
+    const formCreateAcc = document.getElementById("registerForm");
+  
+    if (isLoggedIn()) {
+        hide(formLogin);
+        hide(formCreateAcc);
+    } else {
+    document.addEventListener("click", function (e) {
+      if (e.target.id === "linkRegisterForm") {
+        e.preventDefault();
+        toggleForms(formLogin, formCreateAcc);
+      } else if (e.target.id === "linkLogin") {
+        e.preventDefault();
+        toggleForms(formCreateAcc, formLogin);
+      }
+    });
+}
+  
+    function toggleForms(formToHide, formToShow) {
+        hide(formToHide);
+        show(formToShow);
+    }
 
-  create.addEventListener("click", function (e) {
-    e.preventDefault();
+    function hide(elem) {
+        elem.classList.add("form--hidden");
+        elem.classList.remove("form--unhidden");
+    }
 
-    hide(formLogin);
-    show(formCreateAcc);
+    function show(elem) {
+        elem.classList.add("form--unhidden");
+        elem.classList.remove("form--hidden");
+    }
   });
 
-  haveAcc.addEventListener("click", function (e) {
-    e.preventDefault();
-
-    show(formLogin);
-    hide(formCreateAcc);
-  });
-});
-
-function hide(elem) {
-  elem.classList.add("form--hidden");
-  elem.classList.remove("form--unhidden");
-}
-
-function show(elem) {
-  elem.classList.add("form--unhidden");
-  elem.classList.remove("form--hidden");
-}
-
+  
