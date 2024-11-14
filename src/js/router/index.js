a4c6abc7ac2278777c956e508e99256cc13483c5
gydalo <gylo_l@hotmail.com>
2024-11-14 10:20:17 +0100

    Trying to figure out pathing

--- src/js/router/index.js
+++ src/js/router/index.js
@@ -6,7 +6,7 @@ export default async function router(pathname = window.location.pathname) {
     case "/":
       await import("./views/home.js");
      // await import("./views/buttons.js");
-      await import("./views/login.js");
+      await import("../views/login.js");
       await import("./views/register.js");
       break;
     case "/index.html":
