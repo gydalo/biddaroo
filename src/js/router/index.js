3f64510cc415edc67b6b9d7ccad67da941475f72
gydalo <gylo_l@hotmail.com>
2024-11-25 15:42:02 +0100

    Change the path of the router

--- src/js/router/index.js
+++ src/js/router/index.js
@@ -17,17 +17,17 @@ export default async function router(pathname = window.location.pathname) {
       await import("./views/register.js");
       await import("./views/postCreate.js");
       break;
-      case "/index.html":
-        await import("./views/home.js");
-        await import("./views/buttons.js");
-        await import("./views/login.js");
-        await import("./views/register.js");
-        await import("./views/postCreate.js");
-        break;
+    case "/index.html":
+      await import("./views/home.js");
+      await import("./views/buttons.js");
+      await import("./views/login.js");
+      await import("./views/register.js");
+      await import("./views/postCreate.js");
+      break;
     /*case "/auth":
       await import("./views/auth.js");
       break;  */
-   /* case "/auth/login/":
+    /* case "/auth/login/":
       await import("./views/login.js");
       break;
     case "/auth/login/index.html":
@@ -39,20 +39,32 @@ export default async function router(pathname = window.location.pathname) {
     case "/post/index.html":
       await import("./views/post.js");
       break;
-      case "/biddaroo/post/index.html":
-        await import("./views/post.js");
-        break;
+    case "/biddaroo/post/index.html":
+      await import("./views/post.js");
+      break;
     case "/post/edit/index.html":
+      await import("./views/postEdit.js");
+      break;
+    case "/biddaroo/post/edit/index.html":
       await import("./views/postEdit.js");
       break;
     case "/post/create/index.html":
       await import("./views/postCreate.js");
       break;
+    case "/biddaroo/post/create/index.html":
+      await import("./views/postCreate.js");
+      break;
     case "/profile/index.html":
       await import("./views/profile.js");
       break;
-      case "/profile/":
-        await import("./views/profile.js");
-        break;
+    case "/biddaroo/profile/index.html":
+      await import("./views/profile.js");
+      break;
+    case "/profile/":
+      await import("./views/profile.js");
+      break;
+    case "/biddaroo/profile/":
+      await import("./views/profile.js");
+      break;
   }
 }
